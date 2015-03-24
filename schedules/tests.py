96391e46ddf80f84a07b49c3a45f6549a95bd52c
Karl Floersch <karl.floersch@gmail.com>
2015-03-24 04:12:27 -0400

    Add test

--- schedules/tests.py
+++ schedules/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class LoginTest(unittest.TestCase):
+    def testLogin(self):
+        # Test to see that the login view is working
+        c = Client()
+        response = c.post('/login/', 
+            {'email': 'TestStudent', 'password': 'TestStudent'})
+        response.status_code
+        self.assertEqual(response.status_code, 200)
+
