286e9641931e192d30845d2945a97c4c97180d55
Karl Floersch <karl.floersch@gmail.com>
2015-04-15 17:13:57 -0400

    Add create user validation

--- schedules/views.py
+++ schedules/views.py
@@ -6,11 +6,16 @@ from django.http import HttpResponse
 import datetime
 # import the logging library
 import logging
+# Testing
+from django.views.decorators.csrf import csrf_exempt
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
 def login_view(request):
+    if request.user.is_authenticated():
+        return redirect("/dashboard")
     # Create the data dictionary
     data = {}
     # Get the username and password that was entered
@@ -28,7 +33,6 @@ def login_view(request):
         if user.is_active:
             # If the user has been approved
             login(request, user)
-            logger.info('User: ' + data['username'] + ' logged in')
             return redirect("/dashboard")
             # Redirect to a success page.
         else:
@@ -38,8 +42,6 @@ def login_view(request):
             # Return a 'disabled account' error message
     else:
         # Failed login attempt
-        logger.info('Failed login attempt. Username: ' 
-            + data['username'] + ' | Password: ' + data['password'])
         # If they entered a username then print error
         if data['username'] != '':
             data['errors'] = "IM HEREJ"
@@ -65,7 +67,51 @@ def create_school_view(request):
 
 def create_user_view(request):
     # Display the create user view
-    return render(request, 'create_user.html')
+    if request.method == 'GET':
+        return render(request, 'create_user.html')
+    elif request.method == 'POST':
+        if request.POST.get("request"):
+            is_valid, data = validate_new_user(request)
+            if is_valid:
+                # Data is valid and let's store it in the db
+                redirect("/login")
+            else:
+                return render(request, 'create_user.html', dictionary=data)
+        elif request.POST.get("cancel"):
+            return redirect("/login")
+        return render(request, 'create_user.html')
+
+def validate_new_user(request):
+    data = {}
+    data['studName'] = request.POST.get('studName', False)
+    data['email'] = request.POST.get('email', False)
+    data['school'] = request.POST.get('school', False)
+    data['pw'] = request.POST.get('password', False)
+    data['conf_pw'] = request.POST.get('confirm', False)
+    valid_data = True
+    if len(data['studName'].strip()) == 0:
+        valid_data = False
+        data['err_studName'] = "Invalid name"
+    if validate_email(data['email']):
+        valid_data = False
+        data['err_email'] = "Invalid email"
+    if len(data['school'].strip()) == 0:
+        valid_data = False
+        data['err_school'] = "Invalid school"
+    if len(data['pw'].strip()) == 0:
+        valid_data = False
+        data['err_pw'] = "Invalid password"
+    if data['pw'] == data['conf_pw']:
+        valid_data = False
+        data['err_conf_pw'] = "Passwords didn't match"
+    return valid_data, data
+
+def validate_email( email ):
+    import re
+    a = re.compile("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+    if a.match(email):
+        return False
+    return True
 
 def logout_view(request):
     # Log the user out using Django Auth
