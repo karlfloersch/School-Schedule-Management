97408f7af3fef4410841aa46550776a19e958d6a
christian <maverickx5105@gmail.com>
2015-04-16 20:17:46 -0400

    add multiple friend related quries

--- schedules/db_views.py
+++ schedules/db_views.py
@@ -1,13 +1,137 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
+import pymongo
+import json
+from pymongo import MongoClient
+from bson.dbref import DBRef
+client = MongoClient()
 
 @login_required(redirect_field_name='/login')
-def sample_vew(request):
+def search_for_person_view(request):
     """ render the create school view. TODO: add validation """
     # Display the create school view if the admin is logged in
-    if 'Administrator' in request.user.groups.values_list('name', flat=True):
-        return render(request, 'create_school.html')
-    return redirect("/dashboard")
+    db = client.students
+    #Display all possible people we can add by searching a name
+    username = request.user.username
+    # username = "punk@gmail.com"
+    # Search this person
+    first_name = "punk"
+    bitch_name = "bitch"
+    # find out who i am
+    myself = db.students.find_one({"email":username})
+    # cool i go to this cool
+    school_i_go_to= myself["school"]
+    # lets get all the people with this name and go to the same school as i do
+    people = []
+    # students_list = 
+    for person in db.students.find({"$and":[{"first_name": first_name},{"last_name": last_name},{"school":school_i_go_to}]}):
+        people.append(person)
+    # go to this place
+    # print people
+    all_my_friends_complete = db.dereference(myself["friendslist"])
+    # get the list itself
+    all_my_friends = all_my_friends_complete["list"]
+    # get all the requests assocaited with this person. Both sender or reciever 
+    all_my_requests = []
+    for req in db.friend_requests.find({"$or":[{"email_of_requester":username},{"email_of_emailee":username}]}):
+        all_my_requests.append(req)
 
+    if (not all_my_friends):
+        # this checks if the word non is on the list remove it
+        # this means you have no friends
+        # now we know that we have no friends
+        # this means that we cannot remove it from the list
+        # lets check if we can remove from ppl already requested
+        my_friendslist_id = all_my_friends_complete["_id"]
+        # print my_friendslist_id
+        # db.friends_list.update( { "_id": my_friendslist_id }, { "$pop": { "list": -1 } } ))
+        # print all_my_requests
+        if (not all_my_requests or len(all_my_requests)==0):
+            # well shit there are no requests either
+            # nothing we can do show everything
+            x = ""
+        else:
+            x = ""
+            # we must people - all_my_requests
+            for pe in people:
+                # print str(pe) + "\n"
+                for rq in all_my_friends:
+                    # print str(rq)+"\n"
+                    if (pe["email"] == rq["email_of_requester"] or pe["email"] == rq["email_of_emailee"]):
+                        people.remove(pe)
+                        
+            # requests were made and need to be removed
+    else:
+        # you have friends do something about it
+        # remove all your friends 
+        # print all_my_friends
+        # we must people - all_my_requests
+        for pe in people:
+            # print str(pe) + "\n"
+            for af in all_my_friends:
+                # print str(af)+"\n"
+                if (pe["email"] == af["email"]):
+                    people.remove(pe)
+                    
+
+        if (not all_my_requests or len(all_my_requests)==0):
+            # we found no current requests
+            x = ""
+        else:
+            # we must people - all_my_requests
+            for pe in people:
+                # print str(pe) + "\n"
+                for rq in all_my_requests:
+                    # print str(rq)+"\n"
+                    if (pe["email"] == rq["email_of_requester"] or pe["email"] == rq["email_of_emailee"]):
+                        people.remove(pe)
+
+    # print people
+    # print "success"
+    html = "<html><body> string: "+"success"+"</body></html>"
+    return HttpResponse(html)
+
+@login_required(redirect_field_name='/login')
+def add_a_student_to_friendslist_view(request):
+	db = client.students
+	username = request.user.username
+	myself = db.students.find_one({"email":username})
+	original_id = myself["_id"]
+	first_name_to_be_inserted = "punk"
+	last_name_to_be_inserted = "bitch"
+	email_to_be_inserted = "punk@gmail.com"
+    db.friends_list.update({"_id":original_id},{ "$push": { "list": { "first_name": first_name_to_be_inserted, "last_name": last_name_to_be_inserted, "email": email_to_be_inserted } } })
+    html = "<html><body> string: "+"success"+"</body></html>"
+    return HttpResponse(html)
+
+def send_a_friend_request_view(request):
+	db = client.students
+	email_of_requester="billy@gmail.com"
+    first_name_of_requester="billy"
+    last_name_of_requester="bob"
+    email_of_emailee="charles@gmail.com"
+    name_of_emailee="charles"
+    friend_request_info = {"email_of_requester":email_of_requester,
+    "first_name_of_requester":first_name_of_requester,
+    "last_name_of_requester":last_name_of_requester,
+    "email_of_emailee":email_of_emailee,
+    "name_of_emailee":name_of_emailee}
+    db.friend_requests.insert_one(friend_request_info)
+
+    html = "<html><body> string: "+"success"+"</body></html>"
+    return HttpResponse(html)
+
+def get_friendslist_view(request):
+	email_string = "lazy"
+	fresh_jim = db.students.find_one({"email":email_string})
+    if (fresh_jim is None) or (len(fresh_jim) == 0):
+        # nothing at the moment unless you want something to show up
+    else:
+        print "person's friends are:"
+        some_value = db.dereference(fresh_jim["friendslist"])
+        some_value = some_vale["list"]
+
+    html = "<html><body> string: "+some_value+"</body></html>"
+    return HttpResponse(html)
--- schedules/urls.py
+++ schedules/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     (r'^dashboard/$', views.dashboard_view), # Dash for user and admin
     (r'^create_school/$', views.create_school_view), # Create school for admin
     (r'^create_user/$', views.create_user_view), # New user request page
-    (r'^sample_db_test/$', db_views.sample_vew), # Testing for DB
+    (r'^sample_db_test/$', get_friendslist_view), # Testing for DB
     (r'^$', views.redirect_to_login), # Send user to login
 )
