c2960463d6d3ee3bede19b890075a80e4d5b47c9
Joe Borormeo <Jrunna21@gmail.com>
2015-04-19 16:03:18 -0400

    Add db_view changes

--- schedules/db_views.py
+++ schedules/db_views.py
@@ -2,96 +2,25 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from celery.result import AsyncResult
 import pymongo
 import json
+from schedules.tasks import *
+from celery import Celery
 from pymongo import MongoClient
 from bson.dbref import DBRef
+from . import tasks
 client = MongoClient()
+import time
 
 @login_required(redirect_field_name='/login')
 def search_for_person_view(request):
-	# """ render the create school view. TODO: add validation """
-    # Display the create school view if the admin is logged in
-    db = client.students
-    #Display all possible people we can add by searching a name
-    username = request.user.username
-    # username = "punk@gmail.com"
-    # Search this person
-    first_name = "punk"
-    last_name = "bitch"
-    # find out who i am
-    myself = db.students.find_one({"email":username})
-    # cool i go to this cool
-    school_i_go_to= myself["school"]
-    # lets get all the people with this name and go to the same school as i do
-    people = []
-    # students_list = 
-    for person in db.students.find({"$and":[{"first_name": first_name},{"last_name": last_name},{"school":school_i_go_to}]}):
-        people.append(person)
-    # go to this place
-    # print people
-    all_my_friends_complete = db.dereference(myself["friendslist"])
-    # get the list itself
-    all_my_friends = all_my_friends_complete["list"]
-    # get all the requests assocaited with this person. Both sender or reciever 
-    all_my_requests = []
-    for req in db.friend_requests.find({"$or":[{"email_of_requester":username},{"email_of_emailee":username}]}):
-        all_my_requests.append(req)
+	taskObject_from_task = search_for_student.delay()
+	result = check_task(taskObject_from_task.task_id)
+	return result
 
-    if (not all_my_friends):
-        # this checks if the word non is on the list remove it
-        # this means you have no friends
-        # now we know that we have no friends
-        # this means that we cannot remove it from the list
-        # lets check if we can remove from ppl already requested
-        my_friendslist_id = all_my_friends_complete["_id"]
-        # print my_friendslist_id
-        # db.friends_list.update( { "_id": my_friendslist_id }, { "$pop": { "list": -1 } } ))
-        # print all_my_requests
-        if (not all_my_requests or len(all_my_requests)==0):
-            # well shit there are no requests either
-            # nothing we can do show everything
-            x = ""
-        else:
-            x = ""
-            # we must people - all_my_requests
-            for pe in people:
-                # print str(pe) + "\n"
-                for rq in all_my_friends:
-                    # print str(rq)+"\n"
-                    if (pe["email"] == rq["email_of_requester"] or pe["email"] == rq["email_of_emailee"]):
-                        people.remove(pe)
-                        
-            # requests were made and need to be removed
-    else:
-        # you have friends do something about it
-        # remove all your friends 
-        # print all_my_friends
-        # we must people - all_my_requests
-        for pe in people:
-            # print str(pe) + "\n"
-            for af in all_my_friends:
-                # print str(af)+"\n"
-                if (pe["email"] == af["email"]):
-                    people.remove(pe)
-                    
 
-        if (not all_my_requests or len(all_my_requests)==0):
-            # we found no current requests
-            x = ""
-        else:
-            # we must people - all_my_requests
-            for pe in people:
-                # print str(pe) + "\n"
-                for rq in all_my_requests:
-                    # print str(rq)+"\n"
-                    if (pe["email"] == rq["email_of_requester"] or pe["email"] == rq["email_of_emailee"]):
-                        people.remove(pe)
 
-    # print people
-    # print "success"
-    html = "<html><body> string: "+"success"+"</body></html>"
-    return HttpResponse(html)
 @login_required(redirect_field_name='/login')
 def add_a_student_to_friendslist_view(request):
 	db = client.students
@@ -104,6 +33,57 @@ def add_a_student_to_friendslist_view(request):
 	db.friends_list.update({"_id":original_id},{ "$push": { "list": { "first_name": first_name_to_be_inserted, "last_name": last_name_to_be_inserted, "email": email_to_be_inserted } } })
 	html = "<html><body> string: "+"success"+"</body></html>"
 	return HttpResponse(html)
+
+def check_task(request):
+	async_result = AsyncResult(request)
+	try:
+		result = async_result.get(timeout=30, propagate=False)
+	except TimeoutError:
+		result = None
+	status = async_result.status
+	traceback = async_result.traceback
+	if isinstance(result, Exception):
+		return HttpResponse(json.dumps({
+			'status': status,
+			'error': str(result),
+			'traceback': traceback,
+		}), content_type='application/json')
+	else:
+		return HttpResponse(json.dumps({
+			'status': status,
+			'result': str(result),
+		}), content_type='application/json')
+
+
+
+def test_cel(request):   
+	print ("Supa Duba")
+	#html = add_students_to_database.apply_sync
+	# html = tasks.mul.apply_async((2,2))
+	#task_id = request.POST.get('tasl_')
+	taskObject_from_task = add_students_to_database_two.delay()
+	result = check_task(taskObject_from_task.task_id)
+	
+	#html = taskObject_from_task.get()
+	# result.get()
+	#task_id = result.task_id
+	#print (result.ready())
+	#print (result.status())
+	
+	#result = app.AsyncResult(task_id)
+	#task_id = result.task_id
+	
+	# print (result.state)
+	# result.state
+	# print (results.state)
+	
+	#page = result.get()
+	#print(page)
+	
+
+	#html = AsyncResult(task_id)
+	#html = "<html><body> string: "+str(result)+"</body></html>"
+	return result
 
 def send_a_friend_request_view(request):
 	db = client.students
