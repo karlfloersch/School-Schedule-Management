9740cf78b22ab0e5cbcc9aa2e9796f590ece1cb8
Karl Floersch <karl.floersch@gmail.com>
2015-04-21 02:01:16 -0400

    Add post content for create school

--- static/js/createSchool.js
+++ static/js/createSchool.js
@@ -13,11 +13,11 @@ var semesterTable = function (){
     table.append('<tr><td><b>Name of Semester In Order</b></td>');
 
     for(var i = 0; i < numSemester; i++){
-        table.append('<tr><td><input type="text" id="semester + i"></td>');
+        table.append('<tr><td><input type="text" name="semester_' + i + '"></td>');
     }
   }
   else{
-    $('#err_periods').html("Invalid");
+    $('#err_number_semester').html("Semesters must range from 1-4");
   }
 }
 var periodLunchList = function () {  
@@ -31,11 +31,11 @@ var periodLunchList = function () {
     table.append('<tr><td><b>Valid Lunch Periods</b></td>');
 
     for(var i = 1; i <= numPeriod; i++){
-       table.append('<tr><td>Period '+ i +'<input type="checkbox" id="i" value = "i"></td>');
+       table.append('<tr><td>Period '+ i +'<input type="checkbox" name="lunch_'+ i +'"></td>');
     }
   }
   else{
-    $('#err_periods').html("Invalid");
+    $('#err_periods').html("Periods must range from 6-12");
   }
 }
 function legalBlocksList() {  
@@ -66,19 +66,19 @@ function addToLegalBlocks(){
 
   var days = "";
   if($('#monday').is(':checked')) {
-    days += "M "
+      days += "M,";
   }
   if($('#tuesday').is(':checked')) {
-    days += "Tu "
+      days += "Tu,";
   }
   if($('#wednes').is(':checked')) {
-    days += "W "
+      days += "W,";
   }
   if($('#thursday').is(':checked')) {
-    days += "Th "
+      days += "Th,";
   }
   if($('#friday').is(':checked')) {
-    days += "F"
+      days += "F,";
   }
 
   if(Number(e) > Number(numPeriod)){
@@ -90,9 +90,9 @@ function addToLegalBlocks(){
   else if(Number(s) < Number(e) && s != '' && e != ''){
     $("#legalBlocks tr:last").remove();
 
-    table.append('<tr><td>' + s + '</td>\
-    <td>' + e + '</td>\
-    <td>' + days + '</td>\
+    table.append('<tr><td class="period_start">' + s + '</td>\
+    <td class="period_end">' + e + '</td>\
+    <td class="days_active">' + days.substring(0, days.length - 1) + '</td>\
     <td><input type="button" class="btn" value = "Delete"\
     onClick="Javacsript:deleteListRow(this, this)"></td>');
 
@@ -105,9 +105,6 @@ function addToLegalBlocks(){
       Friday <input type="checkbox" id="friday"<td>\
       <td></td>');
   }
-}
-function start(){
-  legalBlocksList();
 }
  $(function() {
     $( "#dialog-error-number-period" ).dialog({
@@ -133,16 +130,50 @@ function start(){
       }
     });
   });
+function start() {
+    legalBlocksList();
+}
 $( document ).ready(function() {
+  //legalBlocksList();
   $('#numberOfSem').keyup(semesterTable);
   $('#periodInADay').keyup(periodLunchList);
   $('#daysInAcademicYear').keyup(function (){
     var days = $('#daysInAcademicYear').val();
-    if(days < 1 || days > 7){
-      $('#err_day_acad').html("Invalid");
+    if(days < 15){
+      $('#err_day_acad').html("Days much exceed 15");
     }
     else{
       $('#err_day_acad').html("");
     }
-  })
+  });
+    start();
+    console.log("Am i working?");
+    $("#save_btn").click(function(){
+        var start_periods = "";
+        var end_periods = "";
+        var days_active = "";
+        $(".period_start").each(function(i) {
+            start_periods += $(this).text() + " ";
+        });
+        $(".period_end").each(function(i) {
+            end_periods += $(this).text() + " ";
+        });
+        $(".days_active").each(function(i) {
+            days_active += $(this).text() + " ";
+        });
+        $('<input />').attr('type', 'hidden')
+            .attr('name', "start_periods")
+            .attr('value', start_periods)
+            .appendTo('#create_school_form');
+
+        $('<input />').attr('type', 'hidden')
+            .attr('name', "end_periods")
+            .attr('value', end_periods)
+            .appendTo('#create_school_form');
+
+        $('<input />').attr('type', 'hidden')
+            .attr('name', "days_active")
+            .attr('value', days_active)
+            .appendTo('#create_school_form');
+    });
 });
