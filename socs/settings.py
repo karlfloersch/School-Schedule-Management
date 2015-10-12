bdf40fe526914339745d9164b6d29dbe4e49bf7b
Karl Floersch <karl@karlfloersch.com>
2015-10-12 11:16:38 -0400

    Update settings.py

--- socs/settings.py
+++ socs/settings.py
@@ -1,9 +1,6 @@
 """
 Django settings for socs project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
